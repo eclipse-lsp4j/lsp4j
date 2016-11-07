2a209660b832c7f85fab47ca8ba2fd6c65ba1396
Sven Efftinge <sven.efftinge@typefox.io>
2016-11-07 14:31:45 +0100

    removed CancelParams, overloaded runAsync with support for providing an Executor

--- org.eclipse.lsp4j.jsonrpc/src/main/java/org/eclipse/lsp4j/jsonrpc/CompletableFutures.java
+++ org.eclipse.lsp4j.jsonrpc/src/main/java/org/eclipse/lsp4j/jsonrpc/CompletableFutures.java
@@ -9,6 +9,7 @@ package org.eclipse.lsp4j.jsonrpc;
 
 import java.util.concurrent.CancellationException;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executor;
 import java.util.function.Function;
 
 public class CompletableFutures {
@@ -22,10 +23,28 @@ public class CompletableFutures {
 	public static <R> CompletableFuture<R> computeAsync(Function<CancelIndicator, R> code) {
 		CompletableFuture<CancelIndicator> start = new CompletableFuture<>();
 		CompletableFuture<R> result = start.thenApplyAsync(code);
-		start.complete(() -> {
+		CancelIndicator cancelIndicator = () -> {
 			if (result.isCancelled()) 
 				throw new CancellationException();
-		});
+		};
+		start.complete(cancelIndicator);
+		return result;
+	}
+	
+	/**
+	 * A utility method to create a {@link CompletableFuture} with cancellation support.
+	 * 
+	 * @param code a function that accepts a {@link CancelIndicator} and returns the to be computed value
+	 * @return a future
+	 */
+	public static <R> CompletableFuture<R> computeAsync(Executor executor, Function<CancelIndicator, R> code) {
+		CompletableFuture<CancelIndicator> start = new CompletableFuture<>();
+		CompletableFuture<R> result = start.thenApplyAsync(code, executor);
+		CancelIndicator cancelIndicator = () -> {
+			if (result.isCancelled()) 
+				throw new CancellationException();
+		};
+		start.complete(cancelIndicator);
 		return result;
 	}
 	
