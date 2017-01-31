66b8df4a14889aed1112957c3900d25d62ee1074
akosyakov <anton.kosyakov@typefox.io>
2017-01-31 08:09:38 +0500

    Fixes eclipse/lsp4j#58: preserve a stacktrace on the error during the message validation
    Signed-off-by: Christian Dietrich <christian.dietrich@itemis.de>

--- org.eclipse.lsp4j.jsonrpc/src/main/java/org/eclipse/lsp4j/jsonrpc/validation/ReflectiveMessageValidator.java
+++ org.eclipse.lsp4j.jsonrpc/src/main/java/org/eclipse/lsp4j/jsonrpc/validation/ReflectiveMessageValidator.java
@@ -12,6 +12,8 @@ import java.lang.reflect.Modifier;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import java.util.stream.Collectors;
 
 import org.eclipse.lsp4j.jsonrpc.MessageConsumer;
@@ -24,6 +26,8 @@ import com.google.gson.JsonElement;
 
 public class ReflectiveMessageValidator implements MessageConsumer {
 
+	private static final Logger LOG = Logger.getLogger(ReflectiveMessageValidator.class.getName());
+
 	private MessageConsumer delegate;
 
 	public ReflectiveMessageValidator(final MessageConsumer delegate) {
@@ -35,8 +39,9 @@ public class ReflectiveMessageValidator implements MessageConsumer {
 		List<String> result = new ArrayList<>();
 		try {
 			validate(message, result, new LinkedList<>());
-		} catch (Exception e) {
-			result.add("Error during message validation: " + e.getMessage());
+		} catch (Throwable e) {
+			LOG.log(Level.SEVERE, "Error during message validation: " + e.getMessage(), e);
+			result.add("The message validation failed, please look at the server's logs.");
 		}
 		if (!result.isEmpty()) {
 			ResponseError error = new ResponseError(ResponseErrorCode.InvalidParams,
