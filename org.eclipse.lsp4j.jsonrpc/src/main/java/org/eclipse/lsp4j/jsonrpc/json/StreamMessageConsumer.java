3aaee269fd475de66d1326feac69bd2ad0fae1af
Miro Spönemann <miro.spoenemann@typefox.io>
2018-02-06 15:02:32 +0100

    Formatted Java sources for consistent indentation

--- org.eclipse.lsp4j.jsonrpc/src/main/java/org/eclipse/lsp4j/jsonrpc/json/StreamMessageConsumer.java
+++ org.eclipse.lsp4j.jsonrpc/src/main/java/org/eclipse/lsp4j/jsonrpc/json/StreamMessageConsumer.java
@@ -16,72 +16,72 @@ import org.eclipse.lsp4j.jsonrpc.messages.Message;
 
 public class StreamMessageConsumer implements MessageConsumer, MessageConstants {
 
-    private final String encoding;
-    private final MessageJsonHandler jsonHandler;
+	private final String encoding;
+	private final MessageJsonHandler jsonHandler;
 
-    private final Object outputLock = new Object();
-    
-    private OutputStream output;
-    
-    public StreamMessageConsumer(MessageJsonHandler jsonHandler) {
-    	this(null, StandardCharsets.UTF_8.name(), jsonHandler);
-    }
-    
-    public StreamMessageConsumer(OutputStream output, MessageJsonHandler jsonHandler) {
-    	this(output, StandardCharsets.UTF_8.name(), jsonHandler);
-    }
-    
-    public StreamMessageConsumer(OutputStream output, String encoding, MessageJsonHandler jsonHandler) {
-        this.output = output;
-        this.encoding = encoding;
-		this.jsonHandler = jsonHandler;    	    
-    }
-    
-    public OutputStream getOutput() {
-    	return output;
-    }
-    
-    public void setOutput(OutputStream output) {
-    	this.output = output;
-    }
-    
-    @Override
-    public void consume(Message message) {
-        if (message.getJsonrpc() == null) {
-            message.setJsonrpc(JSONRPC_VERSION);
-        }
-        
-        try {
-	        String content = jsonHandler.serialize(message);
-	        byte[] contentBytes = content.getBytes(encoding);
-	        int contentLength = contentBytes.length;
-	        
-	        String header = getHeader(contentLength);
-	        byte[] headerBytes = header.getBytes(StandardCharsets.US_ASCII);
-	        
-	        synchronized (outputLock) {
-	            output.write(headerBytes);
-	            output.write(contentBytes);
-	            output.flush();
-	        }
-        } catch (IOException e) {
-        	throw new RuntimeException(e);
-        }
-    }
-    
-    protected String getHeader(int contentLength) {
-        StringBuilder headerBuilder = new StringBuilder();
-        appendHeader(headerBuilder, CONTENT_LENGTH_HEADER, contentLength).append(CRLF);
-        if (!StandardCharsets.UTF_8.name().equals(encoding)) {
-            appendHeader(headerBuilder, CONTENT_TYPE_HEADER, JSON_MIME_TYPE);
-            headerBuilder.append("; charset=").append(encoding).append(CRLF);
-        }
-        headerBuilder.append(CRLF);
-        return headerBuilder.toString();
-    }
-    
-    protected StringBuilder appendHeader(StringBuilder builder, String name, Object value) {
-        return builder.append(name).append(": ").append(value);
-    }
+	private final Object outputLock = new Object();
+
+	private OutputStream output;
+
+	public StreamMessageConsumer(MessageJsonHandler jsonHandler) {
+		this(null, StandardCharsets.UTF_8.name(), jsonHandler);
+	}
+
+	public StreamMessageConsumer(OutputStream output, MessageJsonHandler jsonHandler) {
+		this(output, StandardCharsets.UTF_8.name(), jsonHandler);
+	}
+
+	public StreamMessageConsumer(OutputStream output, String encoding, MessageJsonHandler jsonHandler) {
+		this.output = output;
+		this.encoding = encoding;
+		this.jsonHandler = jsonHandler;
+	}
+
+	public OutputStream getOutput() {
+		return output;
+	}
+
+	public void setOutput(OutputStream output) {
+		this.output = output;
+	}
+
+	@Override
+	public void consume(Message message) {
+		if (message.getJsonrpc() == null) {
+			message.setJsonrpc(JSONRPC_VERSION);
+		}
+
+		try {
+			String content = jsonHandler.serialize(message);
+			byte[] contentBytes = content.getBytes(encoding);
+			int contentLength = contentBytes.length;
+
+			String header = getHeader(contentLength);
+			byte[] headerBytes = header.getBytes(StandardCharsets.US_ASCII);
+
+			synchronized (outputLock) {
+				output.write(headerBytes);
+				output.write(contentBytes);
+				output.flush();
+			}
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	protected String getHeader(int contentLength) {
+		StringBuilder headerBuilder = new StringBuilder();
+		appendHeader(headerBuilder, CONTENT_LENGTH_HEADER, contentLength).append(CRLF);
+		if (!StandardCharsets.UTF_8.name().equals(encoding)) {
+			appendHeader(headerBuilder, CONTENT_TYPE_HEADER, JSON_MIME_TYPE);
+			headerBuilder.append("; charset=").append(encoding).append(CRLF);
+		}
+		headerBuilder.append(CRLF);
+		return headerBuilder.toString();
+	}
+
+	protected StringBuilder appendHeader(StringBuilder builder, String name, Object value) {
+		return builder.append(name).append(": ").append(value);
+	}
 
 }
--- org.eclipse.lsp4j.jsonrpc/src/main/java/org/eclipse/lsp4j/jsonrpc/json/StreamMessageProducer.java
+++ org.eclipse.lsp4j.jsonrpc/src/main/java/org/eclipse/lsp4j/jsonrpc/json/StreamMessageProducer.java
@@ -23,139 +23,139 @@ import org.eclipse.lsp4j.jsonrpc.messages.Message;
 
 public class StreamMessageProducer implements MessageProducer, Closeable, MessageConstants {
 
-    private static class Headers {
-        int contentLength = -1;
-        String charset = StandardCharsets.UTF_8.name();
-    }
-    
-    private final MessageJsonHandler jsonHandler;
-    
-    private InputStream input;
-    
-    private MessageConsumer callback;
-    private boolean keepRunning;
-    
-    public StreamMessageProducer(InputStream input, MessageJsonHandler jsonHandler) {
-    	this.input = input;
-    	this.jsonHandler = jsonHandler;
-    }
-    
-    public InputStream getInput() {
-    	return input;
-    }
-    
-    public void setInput(InputStream input) {
-    	this.input = input;
-    }
-    
-    @Override
-    public void listen(MessageConsumer callback) {
-        this.callback = callback;
-        keepRunning = true;
-        StringBuilder headerBuilder = null;
-        StringBuilder debugBuilder = null;
-        boolean newLine = false;
-        Headers headers = new Headers();
-        while (keepRunning) {
-            try {
-                int c = input.read();
-                if (c == -1)
-                    // End of input stream has been reached
-                    keepRunning = false;
-                else {
-                    if (debugBuilder == null)
-                        debugBuilder = new StringBuilder();
-                    debugBuilder.append((char) c);
-                    if (c == '\n') {
-                        if (newLine) {
-                            // Two consecutive newlines have been read, which signals the start of the message content
-                            if (headers.contentLength < 0) {
-                                fireError(new IllegalStateException(
-                                    "Missing header " + CONTENT_LENGTH_HEADER + " in input \"" + debugBuilder + "\""
-                                ));
-                            } else {
-                            	boolean result = handleMessage(input, headers);
-                                if (!result)
-                                    keepRunning = false;
-                                newLine = false;
-                            }
-                            headers = new Headers();
-                            debugBuilder = null;
-                        } else if (headerBuilder != null) {
-                            // A single newline ends a header line
-                            parseHeader(headerBuilder.toString(), headers);
-                            headerBuilder = null;
-                        }
-                        newLine = true;
-                    } else if (c != '\r') {
-                        // Add the input to the current header line
-                        if (headerBuilder == null)
-                            headerBuilder = new StringBuilder();
-                        headerBuilder.append((char) c);
-                        newLine = false;
-                    }
-                }
-            } catch (InterruptedIOException e) {
-                // The read operation has been interrupted
-            } catch (ClosedChannelException e) {
-                // The channel whose stream has been listened was closed
-            } catch (IOException e) {
-            	throw new RuntimeException(e);
-            }
-        }
-    }
-	
+	private static class Headers {
+		int contentLength = -1;
+		String charset = StandardCharsets.UTF_8.name();
+	}
+
+	private final MessageJsonHandler jsonHandler;
+
+	private InputStream input;
+
+	private MessageConsumer callback;
+	private boolean keepRunning;
+
+	public StreamMessageProducer(InputStream input, MessageJsonHandler jsonHandler) {
+		this.input = input;
+		this.jsonHandler = jsonHandler;
+	}
+
+	public InputStream getInput() {
+		return input;
+	}
+
+	public void setInput(InputStream input) {
+		this.input = input;
+	}
+
+	@Override
+	public void listen(MessageConsumer callback) {
+		this.callback = callback;
+		keepRunning = true;
+		StringBuilder headerBuilder = null;
+		StringBuilder debugBuilder = null;
+		boolean newLine = false;
+		Headers headers = new Headers();
+		while (keepRunning) {
+			try {
+				int c = input.read();
+				if (c == -1)
+					// End of input stream has been reached
+					keepRunning = false;
+				else {
+					if (debugBuilder == null)
+						debugBuilder = new StringBuilder();
+					debugBuilder.append((char) c);
+					if (c == '\n') {
+						if (newLine) {
+							// Two consecutive newlines have been read, which signals the start of the
+							// message content
+							if (headers.contentLength < 0) {
+								fireError(new IllegalStateException("Missing header " + CONTENT_LENGTH_HEADER
+										+ " in input \"" + debugBuilder + "\""));
+							} else {
+								boolean result = handleMessage(input, headers);
+								if (!result)
+									keepRunning = false;
+								newLine = false;
+							}
+							headers = new Headers();
+							debugBuilder = null;
+						} else if (headerBuilder != null) {
+							// A single newline ends a header line
+							parseHeader(headerBuilder.toString(), headers);
+							headerBuilder = null;
+						}
+						newLine = true;
+					} else if (c != '\r') {
+						// Add the input to the current header line
+						if (headerBuilder == null)
+							headerBuilder = new StringBuilder();
+						headerBuilder.append((char) c);
+						newLine = false;
+					}
+				}
+			} catch (InterruptedIOException e) {
+				// The read operation has been interrupted
+			} catch (ClosedChannelException e) {
+				// The channel whose stream has been listened was closed
+			} catch (IOException e) {
+				throw new RuntimeException(e);
+			}
+		}
+	}
+
 	protected void fireError(Throwable exception) {
 		Logger.getLogger(getClass().getName()).log(Level.SEVERE, exception.getMessage(), exception);
 	}
-	
-    protected void parseHeader(String line, Headers headers) {
-        int sepIndex = line.indexOf(':');
-        if (sepIndex >= 0) {
-            String key = line.substring(0, sepIndex).trim();
-            switch (key) {
-                case CONTENT_LENGTH_HEADER:
-                    try {
-                        headers.contentLength = Integer.parseInt(line.substring(sepIndex + 1).trim());
-                    } catch (NumberFormatException e) {
-                        fireError(e);
-                    }
-                    break;
-                case CONTENT_TYPE_HEADER: {
-                    int charsetIndex = line.indexOf("charset=");
-                    if (charsetIndex >= 0)
-                        headers.charset = line.substring(charsetIndex + 8).trim();
-                    break;
-                }
-            }
-        }
-    }
 
-    protected boolean handleMessage(InputStream input, Headers headers) throws IOException {
-        try {
-            int contentLength = headers.contentLength;
-            byte[] buffer = new byte[contentLength];
-            int bytesRead = 0;
-            
-            while (bytesRead < contentLength) {
-                int readResult = input.read(buffer, bytesRead, contentLength - bytesRead);
-                if (readResult == -1)
-                    return false;
-                bytesRead += readResult;
-            }
-            
-            String content = new String(buffer, headers.charset);
-            Message message = jsonHandler.parseMessage(content);
-            callback.consume(message);
-        } catch (UnsupportedEncodingException | InvalidMessageException e) {
-            fireError(e);
-        }
-        return true;
-    }
-    
-    @Override
-    public void close() {
-        keepRunning = false;
-    }
+	protected void parseHeader(String line, Headers headers) {
+		int sepIndex = line.indexOf(':');
+		if (sepIndex >= 0) {
+			String key = line.substring(0, sepIndex).trim();
+			switch (key) {
+			case CONTENT_LENGTH_HEADER:
+				try {
+					headers.contentLength = Integer.parseInt(line.substring(sepIndex + 1).trim());
+				} catch (NumberFormatException e) {
+					fireError(e);
+				}
+				break;
+			case CONTENT_TYPE_HEADER: {
+				int charsetIndex = line.indexOf("charset=");
+				if (charsetIndex >= 0)
+					headers.charset = line.substring(charsetIndex + 8).trim();
+				break;
+			}
+			}
+		}
+	}
+
+	protected boolean handleMessage(InputStream input, Headers headers) throws IOException {
+		try {
+			int contentLength = headers.contentLength;
+			byte[] buffer = new byte[contentLength];
+			int bytesRead = 0;
+
+			while (bytesRead < contentLength) {
+				int readResult = input.read(buffer, bytesRead, contentLength - bytesRead);
+				if (readResult == -1)
+					return false;
+				bytesRead += readResult;
+			}
+
+			String content = new String(buffer, headers.charset);
+			Message message = jsonHandler.parseMessage(content);
+			callback.consume(message);
+		} catch (UnsupportedEncodingException | InvalidMessageException e) {
+			fireError(e);
+		}
+		return true;
+	}
+
+	@Override
+	public void close() {
+		keepRunning = false;
+	}
 
 }
