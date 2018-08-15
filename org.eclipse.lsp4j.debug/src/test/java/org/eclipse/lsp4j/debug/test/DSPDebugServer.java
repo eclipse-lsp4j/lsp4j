bf0c6d2c5fc767e4130691631104917b470069f9
Jonah Graham <jonah@kichwacoders.com>
2018-08-15 10:26:54 +0100

    Issue #224: New DSPLauncher tests
    To help resolve Issue #224 add some new tests to make sure
    behaviour works as advertised and has some tests of Debug Adapter.
    
    Signed-off-by: Jonah Graham <jonah@kichwacoders.com>

---
+++ org.eclipse.lsp4j.debug/src/test/java/org/eclipse/lsp4j/debug/test/DSPDebugServer.java
@@ -0,0 +1,50 @@
+/*******************************************************************************
+ * Copyright (c) 2018 Kichwa Coders and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package org.eclipse.lsp4j.debug.test;
+
+import java.io.IOException;
+import java.io.PipedInputStream;
+import java.io.PipedOutputStream;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.eclipse.lsp4j.debug.Capabilities;
+import org.eclipse.lsp4j.debug.InitializeRequestArguments;
+import org.eclipse.lsp4j.debug.OutputEventArguments;
+import org.eclipse.lsp4j.debug.launch.DSPLauncher;
+import org.eclipse.lsp4j.debug.services.IDebugProtocolClient;
+import org.eclipse.lsp4j.debug.services.IDebugProtocolServer;
+import org.eclipse.lsp4j.jsonrpc.Endpoint;
+import org.eclipse.lsp4j.jsonrpc.Launcher;
+import org.eclipse.lsp4j.jsonrpc.json.StreamMessageProducer;
+import org.eclipse.lsp4j.jsonrpc.services.ServiceEndpoints;
+import org.eclipse.xtext.xbase.lib.Pair;
+import org.eclipse.xtext.xbase.lib.util.ToStringBuilder;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class DSPDebugServer {
+
+	// This is a test for https://github.com/eclipse/lsp4j/issues/224
+	@Test
+	public void testDebugServerCanBeLaunched() throws IOException {
+		TestDebugServer testDebugServer = new TestDebugServer();
+		Launcher<IDebugProtocolClient> launcher = DSPLauncher.createServerLauncher(testDebugServer,
+				new PipedInputStream(), new PipedOutputStream());
+		Future<Void> listening = launcher.startListening();
+		listening.cancel(true);
+	}
+}
