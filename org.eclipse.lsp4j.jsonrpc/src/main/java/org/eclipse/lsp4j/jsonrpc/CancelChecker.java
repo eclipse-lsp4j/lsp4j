9b99ea5230a3fa47ac7fe86ab413b3778f57d1e3
Miro Spönemann <miro.spoenemann@typefox.io>
2019-08-19 10:13:25 +0200

    #348: Added isCanceled to CancelChecker

--- org.eclipse.lsp4j.jsonrpc/src/main/java/org/eclipse/lsp4j/jsonrpc/CancelChecker.java
+++ org.eclipse.lsp4j.jsonrpc/src/main/java/org/eclipse/lsp4j/jsonrpc/CancelChecker.java
@@ -11,15 +11,29 @@
  ******************************************************************************/
 package org.eclipse.lsp4j.jsonrpc;
 
+import java.util.concurrent.CancellationException;
+
 /**
  * Used for processing requests with cancellation support.
  */
 public interface CancelChecker {
-	
+
 	/**
-	 * Throw a {@link java.util.concurrent.CancellationException} if the currently processed
-	 * request has been canceled.
+	 * Throw a {@link CancellationException} if the currently processed request
+	 * has been canceled.
 	 */
 	void checkCanceled();
-	
+
+	/**
+	 * Check for cancellation without throwing an exception.
+	 */
+	default boolean isCanceled() {
+		try {
+			checkCanceled();
+		} catch (CancellationException ce) {
+			return true;
+		}
+		return false;
+	}
+
 }
