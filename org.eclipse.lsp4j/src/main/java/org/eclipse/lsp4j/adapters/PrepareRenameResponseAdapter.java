efc04e9c21120b0f3c6dd4712ccb5d5b5ae33105
Pierre-Yves B <PyvesDev@gmail.com>
2019-09-09 19:14:07 +0100

    Fix copyright header year range
    Signed-off-by: Pierre-Yves B. <PyvesDev@gmail.com>

--- org.eclipse.lsp4j/src/main/java/org/eclipse/lsp4j/adapters/PrepareRenameResponseAdapter.java
+++ org.eclipse.lsp4j/src/main/java/org/eclipse/lsp4j/adapters/PrepareRenameResponseAdapter.java
@@ -1,5 +1,5 @@
 /******************************************************************************
- * Copyright (c) 2019 Microsoft Corporation and others.
+ * Copyright (c) 2018-2019 Microsoft Corporation and others.
  * 
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0 which is available at
