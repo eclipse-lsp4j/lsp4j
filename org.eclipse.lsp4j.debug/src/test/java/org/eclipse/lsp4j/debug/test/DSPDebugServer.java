1718d8fb936aec58913ab1f741a0dbbef8cb1d56
Titouan Vervack <titouan.vervack@sigasi.com>
2020-02-26 15:00:33 +0100

    Removed unused imports
    Signed-off-by: Titouan Vervack <titouan.vervack@sigasi.com>

--- org.eclipse.lsp4j.debug/src/test/java/org/eclipse/lsp4j/debug/test/DSPDebugServer.java
+++ org.eclipse.lsp4j.debug/src/test/java/org/eclipse/lsp4j/debug/test/DSPDebugServer.java
@@ -14,30 +14,11 @@ package org.eclipse.lsp4j.debug.test;
 import java.io.IOException;
 import java.io.PipedInputStream;
 import java.io.PipedOutputStream;
-import java.util.LinkedHashMap;
-import java.util.Map;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
-import java.util.concurrent.TimeUnit;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
-import org.eclipse.lsp4j.debug.Capabilities;
-import org.eclipse.lsp4j.debug.InitializeRequestArguments;
-import org.eclipse.lsp4j.debug.OutputEventArguments;
 import org.eclipse.lsp4j.debug.launch.DSPLauncher;
 import org.eclipse.lsp4j.debug.services.IDebugProtocolClient;
-import org.eclipse.lsp4j.debug.services.IDebugProtocolServer;
-import org.eclipse.lsp4j.jsonrpc.Endpoint;
 import org.eclipse.lsp4j.jsonrpc.Launcher;
-import org.eclipse.lsp4j.jsonrpc.json.StreamMessageProducer;
-import org.eclipse.lsp4j.jsonrpc.services.ServiceEndpoints;
-import org.eclipse.xtext.xbase.lib.Pair;
-import org.eclipse.xtext.xbase.lib.util.ToStringBuilder;
-import org.junit.After;
-import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 
 public class DSPDebugServer {
