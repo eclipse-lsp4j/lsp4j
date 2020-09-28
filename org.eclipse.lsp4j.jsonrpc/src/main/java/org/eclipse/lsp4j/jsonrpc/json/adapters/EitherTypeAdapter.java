de580dc7a8d09429695d6eae86dffcbe06c6e96d
Guillaume Dufour <guillaume.duff@gmail.com>
2020-09-28 23:39:11 +0200

    fix #435 : parse direct object item instead of list
    Signed-off-by: Guillaume Dufour <guillaume.duff@gmail.com>

--- org.eclipse.lsp4j.jsonrpc/src/main/java/org/eclipse/lsp4j/jsonrpc/json/adapters/EitherTypeAdapter.java
+++ org.eclipse.lsp4j.jsonrpc/src/main/java/org/eclipse/lsp4j/jsonrpc/json/adapters/EitherTypeAdapter.java
@@ -192,9 +192,17 @@ public class EitherTypeAdapter<L, R> extends TypeAdapter<Either<L, R>> {
 		} else if (matchesRight) {
 			// Parse the right alternative from the JSON stream
 			return createRight(right.read(in));
-		} else {
-			throw new JsonParseException("Unexpected token " + nextToken + ": expected " + left + " | " + right + " tokens.");
+		} else if (leftChecker != null || rightChecker != null) {
+			// If result is not the list but directly the only item in the list
+			JsonElement element = new JsonParser().parse(in);
+			if (leftChecker != null && leftChecker.test(element))
+				// Parse the left alternative from the JSON element tree
+				return createLeft(left.read(element));
+			if (rightChecker != null && rightChecker.test(element))
+				// Parse the right alternative from the JSON element tree
+				return createRight(right.read(element));
 		}
+		throw new JsonParseException("Unexpected token " + nextToken + ": expected " + left + " | " + right + " tokens.");
 	}
 	
 	@SuppressWarnings("unchecked")
