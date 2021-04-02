c5d332b43d4f6b582a88a0145ee1d20f611085b5
Jonah Graham <jonah@kichwacoders.com>
2021-04-01 21:05:47 -0400

    Fixes #536: Restore the pre-Jakarta version of websocket bundle (#539)
    Partially reverts "Migrate and Rename Websocket bundle to Jakarta #471 (#487)"
    
    This partially reverts commit f44ee42b1eccb2351a8bc08afe369626c4988dd1.

---
+++ org.eclipse.lsp4j.websocket/src/main/java/org/eclipse/lsp4j/websocket/WebSocketMessageConsumer.java
@@ -0,0 +1,71 @@
+/******************************************************************************
+ * Copyright (c) 2019 TypeFox and others.
+ * 
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0,
+ * or the Eclipse Distribution License v. 1.0 which is available at
+ * http://www.eclipse.org/org/documents/edl-v10.php.
+ * 
+ * SPDX-License-Identifier: EPL-2.0 OR BSD-3-Clause
+ ******************************************************************************/
+package org.eclipse.lsp4j.websocket;
+
+import java.io.IOException;
+import java.util.logging.Logger;
+
+import javax.websocket.Session;
+
+import org.eclipse.lsp4j.jsonrpc.JsonRpcException;
+import org.eclipse.lsp4j.jsonrpc.MessageConsumer;
+import org.eclipse.lsp4j.jsonrpc.json.MessageJsonHandler;
+import org.eclipse.lsp4j.jsonrpc.messages.Message;
+
+/**
+ * Message consumer that sends messages via a WebSocket session.
+ */
+public class WebSocketMessageConsumer implements MessageConsumer {
+
+	private static final Logger LOG = Logger.getLogger(WebSocketMessageConsumer.class.getName());
+	
+	private final Session session;
+	private final MessageJsonHandler jsonHandler;
+	
+	public WebSocketMessageConsumer(Session session, MessageJsonHandler jsonHandler) {
+		this.session = session;
+		this.jsonHandler = jsonHandler;
+	}
+	
+	public Session getSession() {
+		return session;
+	}
+	
+	@Override
+	public void consume(Message message) {
+		String content = jsonHandler.serialize(message);
+		try {
+			sendMessage(content);
+		} catch (IOException exception) {
+			throw new JsonRpcException(exception);
+		}
+	}
+	
+	protected void sendMessage(String message) throws IOException {
+		if (session.isOpen()) {
+			int length = message.length();
+			if (length <= session.getMaxTextMessageBufferSize()) {
+				session.getAsyncRemote().sendText(message);
+			} else {
+				int currentOffset = 0;
+				while (currentOffset < length) {
+					int currentEnd = Math.min(currentOffset + session.getMaxTextMessageBufferSize(), length);
+					session.getBasicRemote().sendText(message.substring(currentOffset, currentEnd), currentEnd == length);
+					currentOffset = currentEnd;
+				}
+			}
+		} else {
+			LOG.info("Ignoring message due to closed session: " + message);
+		}
+	}
+	
+}
