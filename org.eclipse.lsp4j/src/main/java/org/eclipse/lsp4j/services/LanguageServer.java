7512f5dc8137beebb24901b8961f92131938b038
Frederic Gagnon <gagnonfrederic9@gmail.com>
2021-07-04 12:24:29 -0400

    Fix typos in javadocs for LanguageServer (#559)

--- org.eclipse.lsp4j/src/main/java/org/eclipse/lsp4j/services/LanguageServer.java
+++ org.eclipse.lsp4j/src/main/java/org/eclipse/lsp4j/services/LanguageServer.java
@@ -31,24 +31,27 @@ public interface LanguageServer {
 	 * The initialize request is sent as the first request from the client to
 	 * the server.
 	 * 
-	 * If the server receives request or notification before the initialize request it should act as follows:
-	 * 	- for a request the respond should be errored with code: -32001. The message can be picked by the server.
-	 *  - notifications should be dropped, except for the exit notification. This will allow the exit a server without an initialize request.
+	 * If the server receives requests or notifications before the initialize request, it should act as follows:
+	 *  - for a request, the response should be errored with:
+	 *    {@link org.eclipse.lsp4j.jsonrpc.messages.ResponseErrorCode#serverNotInitialized}.
+	 *    The message can be picked by the server.
+	 *  - notifications should be dropped, except for the exit notification.
+	 *    This will allow the client to exit a server without an initialize request.
 	 *  
-	 * Until the server has responded to the initialize request with an InitializeResult 
-	 * the client must not sent any additional requests or notifications to the server.
+	 * Until the server has responded to the initialize request with an InitializeResult,
+	 * the client must not send any additional requests or notifications to the server.
 	 * 
-	 * During the initialize request the server is allowed to sent the notifications window/showMessage, 
-	 * window/logMessage and telemetry/event as well as the window/showMessageRequest request to the client.
+	 * During the initialize request, the server is allowed to send the notifications window/showMessage,
+	 * window/logMessage, and telemetry/event, as well as the request window/showMessageRequest, to the client.
 	 */
 	@JsonRequest
 	CompletableFuture<InitializeResult> initialize(InitializeParams params);
 
 	/**
 	 * The initialized notification is sent from the client to the server after
-	 * the client received the result of the initialize request but before the
+	 * the client received the result of the initialize request, but before the
 	 * client is sending any other request or notification to the server. The
-	 * server can use the initialized notification for example to dynamically
+	 * server can use the initialized notification, for example, to dynamically
 	 * register capabilities.
 	 */
 	@JsonNotification
