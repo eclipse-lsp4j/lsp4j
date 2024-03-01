25dcba2d34f5978615be7d39fdf0aade4193b0cd
sebthom <sebthom@users.noreply.github.com>
2024-03-01 12:52:55 +0100

    refact: remove unused imports

--- org.eclipse.lsp4j.jsonrpc.debug/src/test/java/org/eclipse/lsp4j/jsonrpc/debug/test/json/DebugMessageJsonHandlerTest.java
+++ org.eclipse.lsp4j.jsonrpc.debug/src/test/java/org/eclipse/lsp4j/jsonrpc/debug/test/json/DebugMessageJsonHandlerTest.java
@@ -37,7 +37,6 @@ import org.junit.Test;
 
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
-import com.google.gson.JsonPrimitive;
 import com.google.gson.reflect.TypeToken;
 
 public class DebugMessageJsonHandlerTest {
