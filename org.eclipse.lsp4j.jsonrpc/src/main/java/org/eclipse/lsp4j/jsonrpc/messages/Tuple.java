16e45c4033245ce1b415b675d6bc9710b245cbd6
sebthom <sebthom@users.noreply.github.com>
2024-03-01 12:52:55 +0100

    refact: remove unused imports

--- org.eclipse.lsp4j.jsonrpc/src/main/java/org/eclipse/lsp4j/jsonrpc/messages/Tuple.java
+++ org.eclipse.lsp4j.jsonrpc/src/main/java/org/eclipse/lsp4j/jsonrpc/messages/Tuple.java
@@ -1,13 +1,5 @@
 package org.eclipse.lsp4j.jsonrpc.messages;
 
-import java.io.IOException;
-
-import org.eclipse.lsp4j.jsonrpc.validation.NonNull;
-
-import com.google.gson.TypeAdapter;
-import com.google.gson.stream.JsonReader;
-import com.google.gson.stream.JsonWriter;
-
 /**
  * Representation of tuple types.
  */
