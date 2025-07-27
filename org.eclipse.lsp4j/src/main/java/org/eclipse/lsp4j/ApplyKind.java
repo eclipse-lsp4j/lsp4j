51a2bb76942ed1522f25acdba7e1783410d457d9
Vladimir Piskarev <pisv@1c.ru>
2025-07-27 18:01:04 +0300

    Add beta support for `CompletionList.applyKind`

---
+++ org.eclipse.lsp4j/src/main/java/org/eclipse/lsp4j/ApplyKind.java
@@ -0,0 +1,54 @@
+/******************************************************************************
+ * Copyright (c) 2025 1C-Soft LLC and others.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0,
+ * or the Eclipse Distribution License v. 1.0 which is available at
+ * http://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR BSD-3-Clause
+ ******************************************************************************/
+package org.eclipse.lsp4j;
+
+import org.eclipse.lsp4j.jsonrpc.Draft;
+
+/**
+ * Defines how values from a set of defaults and an individual item will be merged.
+ * <p>
+ * Since 3.18.0
+ */
+@Draft
+public enum ApplyKind {
+	/**
+	 * The value from the individual item (if provided and not {@code null}) will be
+	 * used instead of the default.
+	 */
+	Replace(1),
+
+	/**
+	 * The value from the item will be merged with the default.
+	 * <p>
+	 * The specific rules for merging values are defined against each field
+	 * that supports merging.
+	 */
+	Merge(2);
+
+	private final int value;
+
+	ApplyKind(final int value) {
+		this.value = value;
+	}
+
+	public int getValue() {
+		return value;
+	}
+
+	public static ApplyKind forValue(final int value) {
+		ApplyKind[] allValues = ApplyKind.values();
+		if (value < 1 || value > allValues.length) {
+			throw new IllegalArgumentException("Illegal enum value: " + value);
+		}
+		return allValues[value - 1];
+	}
+}
